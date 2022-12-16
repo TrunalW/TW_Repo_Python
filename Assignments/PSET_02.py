7e4ad7fb7b42a005a78d9eca2d211218108d10ca
TrunalW <trunal.walke@gmail.com>
2022-12-16 16:12:22 +0530

    four Psets committed

---
+++ Assignments/PSET_02.py
@@ -0,0 +1,6 @@
+####Write a python script that reads the name of the user and welcomes him.
+
+user_name = input("Please enter your name\n")
+
+print("Welcome : ",user_name)
+print(f"Welcome: {user_name}")
