95115b6e31dccc77ad27bcd9f6f8e61565deb4d5
TrunalW <trunal.walke@gmail.com>
2023-01-09 19:40:30 +0530

    PSET 8 and 9

---
+++ Assignments/StringAssignments/PSET_08.py
@@ -0,0 +1,7 @@
+""" In one line of code,
+display the result of .find() method for the following strings:
+"""
+txt = input("Enter sample text from table : \n")
+subtxt = input("Enter the sample subtxt from table : \n")
+output = txt.find(subtxt)
+print(f"oneline code  = txt.find(subtxt)  output: {output}")
---
+++ Assignments/StringAssignments/PSET_09.py
@@ -0,0 +1,8 @@
+"""Replace every occurrence of the character "a" with "b"
+"""
+
+str_sample = "An apple a day keeps doctor away"
+
+str_output = str_sample.replace('a','b')
+
+print(f"replacing all 'a' chars with 'b' : {str_output}")
