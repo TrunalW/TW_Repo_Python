3f30b428552285a1098d3e4fc1bc48a2f74406fa
TrunalW <trunal.walke@gmail.com>
2023-01-09 18:01:04 +0530

    rearranging files

--- Assignments/PSET_01.py
+++
@@ -1,5 +0,0 @@
-##Write a python script that reads an input from the std. input and prints the same in the std. output
-
-name = input("Please enter your name \n")
-
-print("Welcome to python world : ", name)
